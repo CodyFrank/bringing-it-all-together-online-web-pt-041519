c2ef08f2db4ba9c7bb21ad4b33c463959e2de48d
Cody Frank <cody.frank30@gmail.com>
2019-06-29 21:22:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -63,7 +63,7 @@ class Dog
     sql = <<-SQL
     SELECT * FROM dogs WHERE name = ? AND breed = ?
     SQL
-    DB[:conn].execute(sql, :name, :breed)
+    dog = DB[:conn].execute(sql, :name, :breed)
     binding.pry
   end
 
