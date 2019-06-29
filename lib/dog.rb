7828c4e173c4eb3280218e267bba1db21184dd9f
Cody Frank <cody.frank30@gmail.com>
2019-06-29 20:39:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,21 @@
 class Dog
   attr_accessor :name, :breed
   attr_reader :id
+
+  
   def initialize(id: nil, name:, breed:)
     @id, @name, @breed = id, name, breed
   end
 
+  def create_table
+    sql = <<-SQL
+    CREATE TABLE dogs(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+    );
+    SQL
+    db[:conn].execute(sql)
+  end
 
 end
